0b68aedac82078f60e8249961f069c5294b45163
Maicon <53847691+maiconsso@users.noreply.github.com>
2023-03-04 14:16:24 -0300

    Acrescentando nova DEF.
    Acrescentando nova def e sistema para ver seu treino após o cadastro.
    usando conceitos de def, lista, dicionário e while. 
    Obs : código com objetivo acadêmico

--- ClientesTreino.py
+++ ClientesTreino.py
@@ -1,11 +1,3 @@
-""" print(f"{'Exercicio':^30} | {'Séries':^6} | {'Repetições':^15}")
-
-
-for pessoa in treino:
-    d = treino[pessoa]
-    print(f"{d['exercicio']:<30} | {d['series']:^6} | {d['repeticoes']:>15}") """
-
-
 treino_h = {
     '0': {'exercicio': '          Treino A', 'series': '-', 'repeticoes': '-'},
     '1': {'exercicio': 'Supino Inclinado com Halteres', 'series': '4', 'repeticoes': '10-12'},
@@ -31,85 +23,73 @@ treino_h = {
 }         
 treino_m = {
     '0': {'exercicio': '          Treino A', 'series': '-', 'repeticoes': '-'},
-    '1': {'exercicio': 'Supino Inclinado com Halteres', 'series': '4', 'repeticoes': '10-12'},
-    '2': {'exercicio': 'Crucifixo na Máquina', 'series': '3', 'repeticoes': '15'},
-    '3': {'exercicio': 'Supino fechado', 'series': '2', 'repeticoes': '8-10'},
-    '4': {'exercicio': 'Remada Alta com Halteres', 'series': '2', 'repeticoes': '8-10'},
-    '5': {'exercicio': 'Tríceps Testa', 'series': '4', 'repeticoes': '8-10'},
+    '1': {'exercicio': 'Agachamento livre', 'series': '3', 'repeticoes': '8'},
+    '2': {'exercicio': 'Leg Press', 'series': '3', 'repeticoes': '8'},
+    '3': {'exercicio': 'Afundo', 'series': '3', 'repeticoes': '8-10'},
+    '4': {'exercicio': 'Cadeira extensora', 'series': '3', 'repeticoes': '8-10'},
+    '5': {'exercicio': 'Cadeira adutora', 'series': '4', 'repeticoes': '8-10'},
     '6': {'exercicio': '           Treino B', 'series': '-', 'repeticoes': '-'},
-    '7': {'exercicio': 'Pulley Frente', 'series': '4', 'repeticoes': '10-12'},
-    '8': {'exercicio': 'Remada Baixa', 'series': '4', 'repeticoes': '10'},
-    '9': {'exercicio': 'Remada Articulada', 'series': '3', 'repeticoes': '8-10'},
-    '10': {'exercicio': 'Elevação Posterior', 'series': '4', 'repeticoes': '8-10'},
-    '11': {'exercicio': 'Rosca 21', 'series': '5', 'repeticoes':'21'},
-    '12': {'exercicio': 'Encolhimento com barra', 'series': '4','repeticoes':'10'},
+    '7': {'exercicio': 'Pulley Frente', 'series': '3', 'repeticoes': '10-12'},
+    '8': {'exercicio': 'Remada na polia', 'series': '3', 'repeticoes': '10'},
+    '9': {'exercicio': 'Supino reto', 'series': '3', 'repeticoes': '8-10'},
+    '10': {'exercicio': 'Voador peitoral', 'series': '3', 'repeticoes': '8-10'},
+    '11': {'exercicio': 'Desenvolvimento', 'series': '3', 'repeticoes':'21'},
+    '12': {'exercicio': 'Elevação lateral', 'series': '3','repeticoes':'10'},
     '13': {'exercicio': '         Treino C', 'series': '-', 'repeticoes': '-'},
-    '14': {'exercicio': 'Passada', 'series': '4', 'repeticoes': '10-12'},
-    '15': {'exercicio': 'Stiff', 'series': '4', 'repeticoes': '10-12'},
-    '16': {'exercicio': 'Leg Press', 'series': '4', 'repeticoes': '10-12'},
-    '17': {'exercicio': 'Mesa Flexora Sentado', 'series': '3', 'repeticoes': '10-12'},
-    '18': {'exercicio': 'Gêmeos em Pé', 'series': '5', 'repeticoes': '10-12'},
-    '19': {'exercicio': 'Elevação de Pernas', 'series': '4', 'repeticoes': '10-12'},
+    '14': {'exercicio': 'Mesa flexora', 'series': '3', 'repeticoes': '10-12'},
+    '15': {'exercicio': 'Stiff', 'series': '3', 'repeticoes': '10-12'},
+    '16': {'exercicio': 'Cadeira flexora', 'series': '4', 'repeticoes': '10-12'},
+    '17': {'exercicio': 'Elevação pélvica', 'series': '3', 'repeticoes': '10-12'},
+    '18': {'exercicio': 'Prancha', 'series': '5', 'repeticoes': '30'},
+    '19': {'exercicio': 'Abdominal reto solo', 'series': '4', 'repeticoes': '10-12'},
 
 }             
-
-Alunos = {
-    'nome':'Maicon',
-    'genero':0,
-    'treino':treino_h,
-}
-
+Alunos =[{'Nome': 'Maicon', 'Genero': 0}]
 
 def CadastrarAlunos():
     dados = {
-        'nome': str (input('Nome completo: ')),
-        'genero':int(input('Masculino = 0, Feminino = 1: ')),
+        'Nome': str(input('Nome completo: ')),
     }
-    nome = dados.get('nome')
-    genero = dados.get('genero')
-    list_nome = [cliente.get('login') for cliente in Alunos]    
+    nome = dados.get('Nome')
+    list_nome = [cliente.get('Nome') for cliente in Alunos]      
     if nome in list_nome:
         return False
     else:
-        Alunos.update(dados)
-        if genero == 0:
-            Alunos:nome['treino'] = treino_h   
-        else:
-            Alunos:nome['treino'] = treino_m
+        Alunos.append(dados)
         return True
-
-def AlunosCadastrados():
-    genero = Alunos.get('genero')
-    list_genero = [cliente.get('genero') for cliente in Alunos]
-    nome = str(input('Nome completo: '))
-    
-    
-    if list_genero in Alunos and genero == 0:
-        print(f'ola {nome}')
         
-  
-
-
-
-while True:
-    print('[2] Já sou cadastrado')
+def See_Training ():
+    treino_Aluno = int (input('Selecione o seu treino:\n0 = Treino Masculino \n1 = Treino Feminino \nNumero :  '))
+    if treino_Aluno == 0:
+        print()
+        print(f"{'Exercicio':^30} | {'Séries':^6} | {'Repetições':^15}")
+                
+        for treino in treino_h:
+            work = treino_h[treino]
+            print(f"{work['exercicio']:<30} | {work['series']:^6} | {work['repeticoes']:>15}")
+    else:
+        print()
+        print(f"{'Exercicio':^30} | {'Séries':^6} | {'Repetições':^15}");
+        for treino in treino_m:
+            work = treino_m[treino]
+            print(f"{work['exercicio']:<30} | {work['series']:^6} | {work['repeticoes']:>15}")    
+    
+while True:    
     print('[1] Cadastrar cliente')
     print('[0] Sair')
     
-
     opcao = int(input('Opção: '))
 
     if opcao == 0:
         break
 
     elif opcao == 1:
-        cadastro = CadastrarAlunos()
-        genero = Alunos('genero')
+        cadastro = CadastrarAlunos()        
         if cadastro is True:
             print("Cliente Cadastrado com sucesso!!")
+            See_Training()
+            break             
         else:
-            print("Cliente já cadastrado!!")
-    elif opcao == 2:
-        AlunosCadastrados()
+            print("Cliente já cadastrado!!")            
     else:
         print("Opção incorreta")
